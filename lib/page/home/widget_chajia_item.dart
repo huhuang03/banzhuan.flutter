a51b9b6fbf28e5bc62f11684c5cbbf4477ce51a2
th <huhuang03@gmail.com>
2020-12-15 15:32:46 +0800

    up

--- lib/page/home/widget_chajia_item.dart
+++ lib/page/home/widget_chajia_item.dart
@@ -1,5 +1,6 @@
 import 'package:banzhuan/chajia/chajia.dart';
 import 'package:banzhuan/depth.dart';
+import 'package:banzhuan/symbol.dart';
 import 'package:flutter/material.dart';
 
 class WidgetChajiaItem extends StatefulWidget {
@@ -14,41 +15,63 @@ class WidgetChajiaItem extends StatefulWidget {
 class _WidgetChajiaItemState extends State<WidgetChajiaItem> {
   bool isExpand = false;
 
+  bool get hasDepth {
+    return this.widget.chajia.hasDepth();
+  }
+
+  Symbol get symbol {
+    return widget.chajia.fromSymbol;
+  }
+
   Widget buildChajia(BuildContext context) {
     var text = "${widget.chajia.fromSymbol.symbol}";
-    if (!widget.chajia.hasDepth()) {
+    if (!hasDepth) {
       text += " depth未获取";
     } else {
       text += " 差价: ${widget.chajia.chajia}";
     }
-    return Text(text);
-  }
 
-  Widget buildOneChajiaTradeItem(BuildContext context, DepthCalcResult result,
-      String prefix) {
-    return ListView(
-      shrinkWrap: true,
-      children: result.items.map((e) => Text("$prefix ${e.price}x${e.amount}")).toList(),
+    return Column(
+      children: [
+        InkWell(
+          child: hasDepth?
+              ExpansionTile(
+                title: Text("${symbol.symbol}差价：${widget.chajia.chajia}"),
+                children: [
+                  buildOneChajiaTradeItem(context, widget.chajia.buy, "买"),
+                  buildOneChajiaTradeItem(context, widget.chajia.sell, "卖"),
+                ],
+              )
+              : Text("${widget.chajia.fromSymbol.symbol} 未获取"),
+          onTap: () {
+            setState(() {
+              if (hasDepth) {
+                this.isExpand = !isExpand;
+              }
+            });
+          },
+        ),
+      ],
     );
   }
 
-  Widget buildChajiaTradeItem(BuildContext context) {
-    if (widget.chajia.hasDepth() && this.isExpand) {
-      return Column(
-        children: [
-          buildOneChajiaTradeItem(context, widget.chajia.buy, "买"),
-          buildOneChajiaTradeItem(context, widget.chajia.sell, "卖"),
-        ],
-      );
-    } else {
-      return Container();
-    }
+  Widget buildOneChajiaTradeItem(
+      BuildContext context, DepthCalcResult result, String prefix) {
+    if (result == null) return Container();
+    return ListView(
+      shrinkWrap: true,
+      children: result.items
+          .map((e) => Text("$prefix ${e.price}x${e.amount}"))
+          .toList(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      buildChajia(context),
-    ],);
+    return Column(
+      children: [
+        buildChajia(context),
+      ],
+    );
   }
 }
